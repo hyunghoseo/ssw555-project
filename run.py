65ce0c7a4804d4185be33983cef30bfcce0c734a
Hyungho Seo <hyunghoseo@gmail.com>
2018-09-17 20:38:32 -0400

    create list of dicts for individuals and families

--- run.py
+++ run.py
@@ -6,40 +6,123 @@ tags = {"0": ["INDI","FAM","HEAD","TRLR","NOTE"],
               "CHIL","DIV"],
         "2": ["DATE"]}
 
+indi = []
+fam = []
+        
 def main(fname):
     with open(fname) as f:
+        entry = {} 
+        type = "" # INDI, FAM
+        expectsDate = 0
+        dateType = "" # birth, death, marr, div
         for line in f.readlines():
             tokens = line.split()
             if len(tokens) == 0:
                 continue
-            print "-->", line.rstrip("\n")
-            valid = "Y"
-            level = tokens[0]
-
-            if len(tokens) > 2 and tokens[2] in ["INDI", "FAM"]:
-                tag = tokens[2]
-                arguments = tokens[1]
-            else:
-                try:
-                    tag = tokens[1]
-                except IndexError:
-                    valid = "N"
-                if tag in ["INDI", "FAM"]:
-                    valid = "N"
-                try:
-                    arguments = " ".join(tokens[2:])
-                except IndexError:
-                    arguments = ""
                 
-            # check that the tag and level are valid
-            if tag not in tags.get(level,[]):
-                valid = "N"
+            level, tag, valid, args = verify_line(tokens)
+            
+            if valid == "N":
+                continue
+            
+            if tag in ["NOTE", "HEAD", "TRLR"]:
+                continue
+            
+            if entry and tag in ["INDI", "FAM"]:
+                if type == "INDI":
+                    indi.append(entry)
+                elif type == "FAM":
+                    fam.append(entry)
+                entry = {}
+                
+            if expectsDate:
+                expectsDate = 0
+                if tag == "DATE":
+                    entry[dateType] = args
+                    continue
+                
+            if not entry.get("id"):
+                if tag == "INDI":
+                    type = "INDI"
+                    entry["id"] = args
+                elif tag == "FAM":
+                    type = "FAM"
+                    entry["id"] = args
+            elif type == "INDI":
+                if tag == "NAME":
+                    entry["name"] = args
+                elif tag == "SEX":
+                    entry["sex"] = args
+                elif tag == "BIRT":
+                    expectsDate = 1
+                    dateType = "birth"
+                elif tag == "DEAT":
+                    expectsDate = 1
+                    dateType = "death"
+                elif tag == "FAMC":
+                    entry["famc"] = args
+                elif tag == "FAMS":
+                    entry["fams"] = args
+            elif type == "FAM":
+                if tag == "MARR":
+                    expectsDate = 1
+                    dateType = "marr"
+                elif tag == "HUSB":
+                    entry["husb"] = args
+                elif tag == "WIFE":
+                    entry["wife"] = args
+                elif tag == "CHIL":
+                    entry["chil"] = args
+                elif tag == "DIV":
+                    expectsDate = 1
+                    dateType = "div"
+           
+                
+                    
+        if type == "INDI":
+            indi.append(entry)
+        if type == "FAM":
+            fam.append(entry)
+            
+        print indi
+        print fam
+            
 
-            print "<-- {}|{}|{}|{}".format(level,tag,valid,arguments)
+def verify_line(tokens):
+    # print "-->", line.rstrip("\n")
+    valid = "Y"
+    level = tokens[0]
+
+    if len(tokens) > 2 and tokens[2] in ["INDI", "FAM"]:
+        tag = tokens[2]
+        arguments = tokens[1]
+    else:
+        try:
+            tag = tokens[1]
+        except IndexError:
+            valid = "N"
+        if tag in ["INDI", "FAM"]:
+            valid = "N"
+        try:
+            arguments = " ".join(tokens[2:])
+        except IndexError:
+            arguments = ""
+        
+    # check that the tag and level are valid
+    if tag not in tags.get(level,[]):
+        valid = "N"
+
+    # print "<-- {}|{}|{}|{}".format(level,tag,valid,arguments)
+    return [level,tag,valid,arguments]
 
 if __name__ == "__main__":
     try:
         fname = sys.argv[1]
     except IndexError:
         print "Usage: run.py [.ged file]"
-    main(fname)
+        exit()
+        
+    try:
+        main(fname)
+    except IOError:
+        print "No such file:", fname
